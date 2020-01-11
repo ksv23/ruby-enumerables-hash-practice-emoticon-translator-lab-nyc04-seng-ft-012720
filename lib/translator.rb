6269c507fe19e0cf943fe7c4314fe296d9b7117b
Kring Villanueva <ksvillanueva7@gmail.com>
2020-01-11 06:02:18 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,12 @@
 # require modules here
+require "yaml"
+emoticons = YAML.load_file('emoticons.yml')
+require 'pry'
 
 def load_library
   # code goes here
+  emoticons
+  binding.pry
 end
 
 def get_japanese_emoticon
